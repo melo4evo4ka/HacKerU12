2a484c80a72c9b69ba4b4c2afc53a3ace00691de
Melo4evo4ka <Melo4evo4ka@rambler.ru>
2019-10-11 21:53:23 +0500

    20191011_2153

---
+++ src/com/company/Airplane.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Airplane {
+    int seatingCatacity;
+    int fightRange;
+    String company;
+
+    public int getSeatingCatacity() {
+        return seatingCatacity;
+    }
+
+    public void setSeatingCatacity(int seatingCatacity) {
+        this.seatingCatacity = seatingCatacity;
+    }
+
+    public int getFightRange() {
+        return fightRange;
+    }
+
+    public void setFightRange(int fightRange) {
+        this.fightRange = fightRange;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Airplane(int seatingCatacity, int fightRange, String company) {
+        this.seatingCatacity = seatingCatacity;
+        this.fightRange = fightRange;
+        this.company = company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Airplane)) return false;
+        Airplane airplane = (Airplane) o;
+        return seatingCatacity == airplane.seatingCatacity &&
+                fightRange == airplane.fightRange;
+
+    }
+
+  /*  @Override
+    public int hashCode() {
+        return Objects.hash(seatingCatacity, fightRange, company);
+    }
+*/
+    public static void main(String[] args) {
+        Airplane S7 = new Airplane(100, 2003,"S7");
+        Airplane UralAirlaines = new Airplane(100, 2003, "UralAirlines");
+        System.out.println(S7.equals(UralAirlaines));
+    }
+}
