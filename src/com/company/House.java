e4cc4a72ade1c7701c8ec5f533267bea42d929c9
Melo4evo4ka <Melo4evo4ka@rambler.ru>
2019-10-15 18:21:15 +0500

    Исправил 20191015

--- src/com/company/House.java
+++ src/com/company/House.java
@@ -4,7 +4,6 @@ public class House {
     String color;
     int height;
 
-
     public String getColor() {
         return color;
     }
@@ -23,30 +22,36 @@ public class House {
     }
 
     // TODO метод не учитывает поле COLOR
-    public static int coast(int height)
+        public static int coast(int height, String color)
     {
-        //рассчтитать стоимость дома по пораметрам COLOR  и height
-        int coast = 40;
-        coast = height*coast;
+        int c; //коэффициэнт стоимости цвета
+        int coast = 40; //стомсость кв.м
+        switch (color){
+            case "lightred" : c = 20;
+            break;
+            case "lightgreen" : c = 30;
+            break;
+            case "lightblue" : c = 40;
+            break;
+            case "lightgray" : c = 50;
+            break;
+            case "lightblack" : c = 60;
+            break;
+            default: c = 10;
+            break;
+        }
+        coast = height*coast*c;
         return coast;
     }
-
-
-
     public static void main(String[] args) {
-
         House dom1 = new House();
         House dom2 = new House();
-
         dom1.setColor("green");
         dom1.setHeight(17);
         dom2.setColor("red");
         dom2.setHeight(17);
-
-
-        System.out.println(dom1.getColor() + " " + dom1.getHeight() + " " + dom1.coast(dom1.height));
-        System.out.println(dom2.getColor() + " " + dom2.getHeight() + " " + dom2.coast(dom2.height));
-
+        System.out.println(dom1.getColor() + " " + dom1.getHeight() + " " + dom1.coast(dom1.height,dom1.color));
+        System.out.println(dom2.getColor() + " " + dom2.getHeight() + " " + dom2.coast(dom2.height,dom2.color));
     }
 
 }
