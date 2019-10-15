0873d7b1b9c217200693b991dba0b6d49f91130a
Melo4evo4ka <Melo4evo4ka@rambler.ru>
2019-10-15 19:59:38 +0500

    20191015 19-59

--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,12 +1,24 @@
 package com.company;
-
+/*
+- Создать 3 статических метода method1(), method2(), method3()
+- Метод main должен вызывать method1(), метод method1() должен вызывать method2(), метод method2() должен вызывать method3()
+- В конце каждого метода, в том числе и main, необходимо добавить sout с выводом названия метода, в котором он расположен
+- Запустить приложение и посмотреть в консоли в каком порядке вызываются методы Вывод в консоль должен получиться такой:
+method3
+method2
+method1
+main
+- Расставить точки останова в каждом методе, запустить в Debug режиме и на каждом шаге наблюдать как ведет себя стэк вызовов
+* вызвать ошибку stackOverFlow и вызвать ошибку OutOfMemory
+ */
 public class Stack {
     public static void main(String[] args) {
-    method1();
+        method1();
         System.out.println("main");
     }
 
     public static void method1() {
+        //method1(); Exception in thread "main" java.lang.StackOverflowError
         method2();
         System.out.println("method1");
 
@@ -17,6 +29,7 @@ public class Stack {
 
     }
     public static void method3 () {
-
+        System.out.println("method3");
+        //method2();
     }
 }
